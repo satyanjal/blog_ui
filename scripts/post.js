fa98fddf16869e711201666b3081f0424239ffdc
Satyanjal <satyanjal.prakash@analyttica.com>
2020-06-08 21:30:57 +0530

    final changes for moving from post list to post html page

--- scripts/post.js
+++ scripts/post.js
@@ -1,3 +1,47 @@
+var data = {}
+window.onload = function () {
+    var url = document.location.href,
+        params = url.split('?')[1].split('&'),
+        tmp;
+    for (var i = 0, l = params.length; i < l; i++) {
+         tmp = params[i].split('=');
+         data[tmp[0]] = decodeURI(tmp[1]);
+    }
+
+    var template = '<div class="container">' +
+        '<p class="header-design" id="user-header">'+ data.content_heading +'</p>'+
+        '<div class="name-edit">' +
+            '<span class="float-left name-alignment">'+ data.name +'</span>'+
+            '<span class="float-right" id="edit-btn">'+
+                '<button class="btn-danger" onclick="editContent()">Edit <i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>'+
+            '</span>'+
+        '</div>'+
+        '<div>'+
+            '<p id="user-content">'+ data.content_body +'</p>'+
+            '<button class="btn-danger" onclick="liked()" id="like-name">'+
+                '<i class="fa fa-thumbs-up" aria-hidden="true"></i>'+
+                '<span >Like</span>'+
+            '</button>'+
+            '<p id="like-content">Be the first one to like this!</p>'+
+        '</div>'+
+        '<div>'+
+            '<textarea class="textarea-design" id="comment-text" placeholder="Leave a comment..."></textarea>'+
+            '<div><button class="btn-danger" onclick="addComment()">Comment</button></div>'+
+        '</div>'+
+        '<div>'+
+            '<p>All Comments</p>'+
+            '<span id="all-comments">'+
+            '</span>'+
+        '</div>'+
+    '</div>';
+
+    document.getElementById('main-content').innerHTML = template;
+
+}
+
+
+
+
 function liked() {
     document.getElementById('like-content').innerHTML = '1 person likes this!';
     document.getElementById('like-name').innerHTML = '<i class="fa fa-thumbs-up" aria-hidden="true"></i> Liked';
--- scripts/postslist.js
+++ scripts/postslist.js
@@ -7,28 +7,28 @@ function deleteModal(userCardId){
 var postListObject = [
     {
         name: "Srishti Gupta",
-        content_heading : "'let' me be a 'const'(ant), not a 'var'(iable)!",
-        content_body : "Since JavaScript does not have any type-checking, either of these keywords can be used to declare a variable of any type (datatype) in JavaScript. Though all the three keywords are used for the same purpose, they are different."
+        content_heading : "let me be a const(ant) not a var(iable)!",
+        content_body : "Since JavaScript does not have any type checking either of these keywords can be used to declare a variable of any type (datatype) in JavaScript. Though all the three keywords are used for the same purpose they are different."
     },
     {
         name: "Colby Fayock",
-        content_heading : "What is linting and how can it save you time?",
-        content_body : "One of the biggest challenges in software development is time. It's something we can't easily get more of, but linting can help us make the most out of the time we have."
+        content_heading : "What is linting and how can it save you time",
+        content_body : "One of the biggest challenges in software development is time. It's something we can't easily get more of it but linting can help us make the most out of the time we have."
     },
     {
         name: "Yazeed Bzadough",
         content_heading : "How to Get More Views on Your Tech Blog",
-        content_body : "everyone and their cat start a blog, YouTube channel, or Patreon. We all want to become stars, or at the very least, a recognizable name in the industry."
+        content_body : "everyone and their cat start a blog YouTube channel or Patreon. We all want to become stars or at the very least a recognizable name in the industry."
     },
     {
         name: "Cedd Burge",
         content_heading : "How to write easily describable code",
-        content_body : "When code is not describable using words, most people have to do some mental mapping to turn it in to words. This wastes mental energy, and you run the risk of getting the mapping wrong. Different people will map to different words, which leads to confusion when discussing the code."
+        content_body : "When code is not describable using words most people have to do some mental mapping to turn it in to words. This wastes mental energy and you run the risk of getting the mapping wrong. Different people will map to different words which leads to confusion when discussing the code."
     },
     {
         name: "Srishti Gupta",
-        content_heading : "Everything you should know about 'module' & 'require' in Node.js",
-        content_body : "Node.js treats each JavaScript file as a separate module. For instance, if you have a file containing some code and this file is named xyz.js, then this file is treated as a module in Node, and you can say that you’ve created a module named xyz."
+        content_heading : "Everything you should know about module and require in Node.js",
+        content_body : "Node.js treats each JavaScript file as a separate module. For instance if you have a file containing some code and this file is named xyz.js then this file is treated as a module in Node and you can say that you have created a module named xyz."
     }
 ];
 
@@ -39,18 +39,18 @@ function showProject(){
         var userCardId = 'usercard_'+idName;
         var template = '<span id="'+ userCardId +'"><div class="input-group mb-3 card-design">' +
         '<div class="input-group-prepend">' +
-            '<p class="input-group-text name-design">' + value.name + '</p>' +
+            '<p class="input-group-text name-design" id="'+ userCardId +'_name">' + value.name + '</p>' +
         '</div>' +
 
         '<div class="form-control content-design">' +
         '<div>' +
-            '<p class="content-heading">' + value.content_heading +
+            '<p class="content-heading"><span id="'+ userCardId +'_content_heading">' + value.content_heading + '</span>' +
                 '<span class="float-right cursor"><a onclick="deleteModal('+ userCardId +')"><i class="fa fa-trash" aria-hidden="true"></i></a></span>'+
             '</p>'+
         '</div>' +
-        '<p class="content-body">' + value.content_body + '</p>' +
+        '<p class="content-body" id="'+ userCardId +'_content_body">' + value.content_body + '</p>' +
         '<span>' +
-            '<span class="float-right cursor"><a href="./post.html"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a></span>' +
+            '<span class="float-right cursor"><a onclick="gotopost(' + userCardId + ')"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a></span>' +
         '</span></div></div></span>';
 
          document.getElementById("card-details").innerHTML += template;
@@ -63,3 +63,18 @@ function deleteUserCard(userCardId) {
     document.getElementById(userCardId.id).innerHTML = '';
     $('#deleteModal').modal('hide');
 }
+
+function gotopost(usercardId) {
+    var name_id = usercardId.id + '_name';
+    var content_heading_id = usercardId.id + '_content_heading';
+    var content_body_id = usercardId.id + '_content_body';
+    var name = document.getElementById(name_id).innerHTML;
+    var content_heading = document.getElementById(content_heading_id).innerHTML;
+    var content_body = document.getElementById(content_body_id).innerHTML;
+    var url = '../html/post.html?name=' + encodeURIComponent(name) + '&content_heading=' + encodeURIComponent(content_heading) + '&content_body=' + encodeURIComponent(content_body);
+    document.location.href = url;
+}
+
+
+
+
